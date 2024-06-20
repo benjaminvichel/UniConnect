6fc718a502bd1a3ef7d6ce2db7374ced4cff9fe4
Benjamin V <you@example.com>
2024-06-20 15:56:33 -0300

    Implemented CSS, HTML, and JavaScript styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -9,14 +9,11 @@ export const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState('Login');
 
     const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (email && password) {
-            setLoading('working...')
             const isLogged = await auth.signin(email, password);
-            setLoading('login')
             if (isLogged) {
                 navigate('/');
             } else {
@@ -31,11 +28,11 @@ export const Login = () => {
             <div className="appLogin">
                 <form onSubmit={handleLogin}>
                     <h1>Login</h1>
-                    <div className="login_input"><input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder="digite seu email" /></div>
-                    <div className="login_input"><input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="digite sua senha" /></div>
+                    <div className="login_input"><input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder="Digite seu email" /></div>
+                    <div className="login_input"><input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Digite sua senha" /></div>
                     <div className="login_forgotPassword"><Link to="/UniConnect/ForgotPassword" className="login_forgotPasswordLink">Esqueceu email ou senha?</Link></div>
-                    <div className="login_button"><button type="submit">{loading}</button></div>
-                    <div className="login_register"><p>Não tem conta?</p><Link to="/UniConnect/Register" className="login_registerLink">Register</Link></div>
+                    <div className="login_button"><button type="submit">Entrar</button></div>
+                    <div className="login_register"><p>Não tem conta?</p><Link to="/UniConnect/Register" className="login_registerLink">Registrar</Link></div>
                 </form>
 
             </div>
