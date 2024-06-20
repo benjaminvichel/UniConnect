9558954697ba682bef39df604e75edb8adbc7a29
Benjamin V <you@example.com>
2024-06-20 15:42:26 -0300

    Deleted uploadImage. Created pendingUser and pendingUsers for users awaiting approval. Created methods to PDF viewing

--- src/context/auth/AuthContext.tsx
+++ src/context/auth/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { User } from '../../types/User';
+import { User, role } from '../../types/User';
 import { Job, WorkStyle, EmploymentType } from '../../types/Job';
 import { State } from '../../types/State';
 
@@ -8,12 +8,16 @@ export type AuthContextType = {
     signin: (email: string, password: string) => Promise<boolean>;
     signout: () => void;
     register: (name: string, email: string, password: string, phone: string, address: string, city: string, state: number) => Promise<boolean>;
-    uploadImage: (image: File) => Promise<boolean>;
+    pendingUser: () => Promise<User[] | null>;
+    pendingUsers: User[] | null;
+    ApprovedOrDeniedUsers: (id: number, role: role, approvalStatus: string) => Promise<boolean>;
+    uploadPDF: (PDF: File) => Promise<boolean>;
+    getPDF: () => Promise<Blob | null>;
     jobs: Job[] | null;
     getJobsList: () => Promise<Job[] | null>;
     userJobs: Job[] | null;
     getUserJobsList: () => Promise<Job[] | null>;
-    addJob: (title: string, text: string, workStyle: WorkStyle, employmentType: EmploymentType, description: string, promoter: string, salary: number, city: string, stateId: number | null, stateName: string | null) => Promise<boolean>;
+    addJob: (title: string, workStyle: WorkStyle, employmentType: EmploymentType, description: string, promoter: string, salary: number, city: string, stateId: number | null, stateName: string | null) => Promise<boolean>;
     subscribeToJob: (id: number) => void;
     removeJob: (id: number) => Promise<boolean>;
     //apiStates
